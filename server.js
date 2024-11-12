8aa4d083892703aed718d58603dde2c073df7cc9
Umaid Ismail <umaidismail@umaids-air.mynetworksettings.com>
2024-11-12 13:16:53 -0800

    initial commit

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
 // TODO: this file :)
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const employees = require("./employees");
+
+app.get("/", (req, res) => {
+  res.send("hello employees");
+});
+
+app.get("/employees", (req, res) => {
+  res.json(employees);
+});
+
+app.get("/employees/random", (req, res) => {
+  const randomIndex = Math.floor(Math.random() * employees.length)
+  res.json(employees[randomIndex]);
+  
+});
+
+app.get("/employees/:index", (req, res) => {
+  const {index} = req.params;
+  if(index < 0 || index >= employees.length){
+    res.status(404).send("that employee does not exist");
+  }else {
+    res.json(employees[index]);
+  }
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`The server is listening on port #${PORT}.`);
+});
